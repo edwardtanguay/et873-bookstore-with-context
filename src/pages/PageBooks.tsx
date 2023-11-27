d43423974d42daf9ce2d161f83cd1f1bf51d2599
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-27 11:53:52 +0100

    books showing

--- src/pages/PageBooks.tsx
+++ src/pages/PageBooks.tsx
@@ -2,8 +2,24 @@ import { useContext } from "react";
 import { AppContext } from "../AppContext";
 
 export const PageBooks = () => {
-	const { userName } = useContext(AppContext);
+	const { userName, books } = useContext(AppContext);
 	return (
-		<>{userName && <p>Hallo, {userName}, please select your books!</p>}</>
+		<>
+			{userName && <p>Hallo, {userName}, please select your books!</p>}
+
+			<p className="mb-4">There are {books.length} books.</p>
+
+			<div className="flex gap-3 flex-wrap">
+				{books.map((book) => {
+					return (
+						<img
+							key={book.id}
+							className="w-40 h-fit"
+							src={`https://edwardtanguay.vercel.app/share/images/techBooks/${book.idCode}.jpg`}
+						/>
+					);
+				})}
+			</div>
+		</>
 	);
 };
