6be7283c63fcffb3e0c51107129e191226f2cdd5
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-27 11:02:55 +0100

    input html in

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,10 +1,15 @@
-import { useContext } from 'react';
-import { AppContext } from '../AppContext';
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
 
 export const PageWelcome = () => {
 	const { userName } = useContext(AppContext);
 
 	return (
-		<p>Welcome, {userName}!</p>
-	)
-}
+		<>
+			<p>Welcome, {userName}!</p>
+			<div>
+				<input className="p-2 rounded mt-3" type="text" /> <button>Add name</button>
+			</div>
+		</>
+	);
+};
