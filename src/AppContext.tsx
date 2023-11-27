1feba5e1313be7a837e465c1b29796e739f49e42
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-27 20:05:24 +0100

    finished2

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -22,20 +22,20 @@ export const AppContext = createContext<IAppContext>({} as IAppContext);
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 	const [userName, setUserName] = useState("");
 	const [books, setBooks] = useState<IBook[]>([]);
-	const [cart, setCart] = useState<ICart>({items:[]} as ICart);
+	const [cart, setCart] = useState<ICart>({ items: [] } as ICart);
 
 	useEffect(() => {
-		(async () => {
+		setTimeout(async () => {
 			const response = await axios.get(booksUrl);
 			const _books = response.data;
 			setBooks(_books);
-		})();
+		}, 2000);
 	}, []);
 
 	const handleAddBookToCart = (book: IBook) => {
 		const _cart = structuredClone(cart);
 		_cart.items.push(book);
-		setCart(_cart);	
+		setCart(_cart);
 	};
 
 	return (
